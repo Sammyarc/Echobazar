9ae1d11ff49f6336107f33ee08940deb0397ad1f
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2024-12-07 13:19:00 +0100

    Modifications to the footer links to point individually to different pages

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,16 +7,72 @@ const Footer = () => {
     const footerSections = [
         {
             title: 'Account',
-            links: ['My Account', 'Order History', 'Cart', 'Wishlist']
+            links: [
+                {
+                    name: 'My Account',
+                    url: '/my-account'
+                }, {
+                    name: 'Order History',
+                    url: '/my-account/orders'
+                }, {
+                    name: 'Cart',
+                    url: '/cart'
+                }, {
+                    name: 'Wishlist',
+                    url: '/my-account/wishlist'
+                }
+            ]
         }, {
             title: 'Help',
-            links: ['Contact', 'Faqs', 'Terms & Condition', 'Privacy Policy']
+            links: [
+                {
+                    name: 'Contact',
+                    url: '/contact'
+                }, {
+                    name: 'Faqs',
+                    url: '/faqs'
+                }, {
+                    name: 'Terms & Condition',
+                    url: '/terms'
+                }, {
+                    name: 'Privacy Policy',
+                    url: '/privacy-policy'
+                }
+            ]
         }, {
             title: 'Proxy',
-            links: ['About', 'Shop', 'Product', 'Track Order']
+            links: [
+                {
+                    name: 'About',
+                    url: '/about'
+                }, {
+                    name: 'Shop',
+                    url: '/shop'
+                }, {
+                    name: 'Product',
+                    url: '/product'
+                }, {
+                    name: 'Track Order',
+                    url: '/track-order'
+                }
+            ]
         }, {
             title: 'Categories',
-            links: ['Fruit & Vegetables', 'Meat & Fish', 'Bread & Bakery', 'Beauty & Health']
+            links: [
+                {
+                    name: 'Fruit & Vegetables',
+                    url: '/categories/fruit-vegetables'
+                }, {
+                    name: 'Meat & Fish',
+                    url: '/categories/meat-fish'
+                }, {
+                    name: 'Bread & Bakery',
+                    url: '/categories/bread-bakery'
+                }, {
+                    name: 'Beauty & Health',
+                    url: '/categories/beauty-health'
+                }
+            ]
         }
     ];
 
@@ -30,13 +86,16 @@ const Footer = () => {
                     <div className="md:w-[25%]">
                         {/* logo */}
                         <img src={Logo} className='w-[40vw] h-[10vw] md:w-[15vw] md:h-[5vw]'/>
-                        <p className="text-[4vw] md:text-[1vw] mb-3 mt-[1.5vw] md:mt-[0vw] font-Poppins">
+                        <p
+                            className="text-[4vw] md:text-[1vw] mb-3 mt-[1.5vw] md:mt-[0vw] font-Poppins">
                             Shop fresh, quality food delivered to your door. We make grocery shopping
                             simple, so you can enjoy more of what matters.
                         </p>
                         <div className="flex items-center gap-3 mb-2">
-                            <span className="border-b-2 text-[4vw] md:text-[1vw] font-Poppins border-Primary pb-[0.5vw]">(219) 555-0114</span>
-                            <span className="text-gray-500 font-Poppins pb-[0.5vw] text-[4vw] md:text-[1vw]">or</span>
+                            <span
+                                className="border-b-2 text-[4vw] md:text-[1vw] font-Poppins border-Primary pb-[0.5vw]">(219) 555-0114</span>
+                            <span
+                                className="text-gray-500 font-Poppins pb-[0.5vw] text-[4vw] md:text-[1vw]">or</span>
                             <a
                                 href="mailto:proxy@gmail.com"
                                 className="text-Primary font-Poppins pb-[0.5vw] text-[4vw] md:text-[1vw] border-b-2 border-Primary">proxy@gmail.com</a>
@@ -44,24 +103,24 @@ const Footer = () => {
                     </div>
 
                     {/* Links sections */}
-                    <div className="grid grid-cols-2 gap-4 md:gap-20 md:grid-cols-4 mt-[5vw] md:mt-[2vw]">
+                    <div
+                        className="grid grid-cols-2 gap-4 md:gap-20 md:grid-cols-4 mt-[5vw] md:mt-[2vw]">
                         {
                             footerSections.map((section) => (
                                 <div key={section.title} className="flex flex-col">
-                                    <h3 className="text-white font-semibold mb-2 font-Poppins text-[4.5vw] md:text-[1vw] ">{section.title}</h3>
+                                    <h3
+                                        className="text-white font-semibold mb-2 font-Poppins text-[4.5vw] md:text-[1vw] ">{section.title}</h3>
                                     <ul>
                                         {
                                             section
                                                 .links
                                                 .map((link) => (
-                                                    <li key={link} className="mb-2">
-                                                        <Link to={`/${link
-                                                                .replace(/\s+/g, '-')
-                                                                .toLowerCase()}`}
-                                                                onClick={() => window.scrollTo(0,0)}
-                                                            // Creates a slug for each link
-                                                            className="hover:text-Primary text-[4vw] md:text-[0.9vw]  transition-colors font-Poppins">
-                                                            {link}
+                                                    <li key={link.name} className="mb-2">
+                                                        <Link to={link.url}
+                                                            // Using the URL defined in the array
+                                                            onClick={() => window.scrollTo(0, 0)} className="hover:text-Primary text-[4vw] md:text-[0.9vw] transition-colors font-Poppins">
+                                                            {link.name}
+                                                            {/* Display the link name */}
                                                         </Link>
                                                     </li>
                                                 ))
