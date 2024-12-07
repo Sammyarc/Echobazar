2bb629afb2804a4a784d3013cc2194d3b0b00dd1
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2024-12-07 13:14:34 +0100

    Added routes to get, and post orders made by users

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createOrder, sendOrderConfirmationEmail } from "../controllers/order.controller.js";
+import { createOrder, getOrdersByUser, sendOrderConfirmationEmail } from "../controllers/order.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-// Get all products
+// Post all orders to the backend and send confirmation email
 router.post('/orders', verifyToken, createOrder, sendOrderConfirmationEmail);
+
+// Get order posted by a specific user
+router.get("/user/orders", verifyToken, getOrdersByUser)
 
 export default router;
