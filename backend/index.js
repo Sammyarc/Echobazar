315e15647b291c47cfc2a40ce9ab3fec74eaf8a5
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2024-12-09 17:56:14 +0100

    Modifications

--- backend/index.js
+++ backend/index.js
@@ -12,14 +12,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    process.env.FRONTEND_URL, // Hosted frontend from .env
-  ];
   
   app.use(
     cors({
-      origin: allowedOrigins,
+      origin: process.env.FRONTEND_URL || 'http://localhost:5000',
       credentials: true,
     })
   );
