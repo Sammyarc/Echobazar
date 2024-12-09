8b0ef0528f66b6e25961b3b7d1cbf2f0f0b7c69f
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2024-12-09 16:45:16 +0100

    Deployment changes

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,6 @@ app.use("/api/auth", authRoutes);
 app.use('/api', productRoutes);
 app.use('/api/create', orderRoutes);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    })
-}
 
 app.listen(PORT, () => {
     connectDB();
