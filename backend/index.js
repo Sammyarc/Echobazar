595e362f97c506811eb43dafa360b0807182851d
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2024-12-10 14:27:38 +0100

    Cors issues fixed

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-  
-  app.use(
-    cors({
-      origin: process.env.FRONTEND_URL || 'http://localhost:5000',
-      credentials: true,
-    })
-  );
+const allowedOrigins = [
+  "http://localhost:5173", // Dev
+  "https://echobazar-ecommerce.vercel.app", // Prod
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
